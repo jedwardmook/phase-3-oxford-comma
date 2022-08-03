f987de60c15517ba1b9b5dadb56e344afbb33e06
JedwardMook <jedwardmook@yahoo.com>
2022-08-03 12:10:40 -0500

    commit tree 87b3e8393ca6078f429ebedf69b64188f64f40f1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+    if (array.length == 1)
+        array.join
+    elsif (array.length == 2)
+        array.join(" and ")
+    else
+        array.map do |item|
+            if item == array[-1]
+                array[-1] = "and " << item
+            end
+        end
+        array.join(", ")
+    end
 
 end
